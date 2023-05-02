7fc85a378bc07096eb0bcaccbfd8567485b34228
Daniel Fischesser <dfischesser@gmail.com>
2023-05-01 19:40:23 -0700

    removed json ignore

--- FoodItem.cs
+++ FoodItem.cs
@@ -5,8 +5,6 @@ namespace Pizza
     public class FoodItem
     {
         public int? FoodID { get; set; }
-
-        [JsonIgnore]
         public int? MenuCategoryID { get; set; }
         public string? FoodName { get; set; }
     }
